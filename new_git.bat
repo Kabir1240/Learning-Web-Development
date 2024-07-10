fc856f7a420c585be6995b6f48335afdce7a02da
Kabir Kashif <kashif.kabir60@gmail.com>
2024-07-10 13:18:46 +0500

    added CSS projects

--- new_git.bat
+++ new_git.bat
@@ -1,7 +1,7 @@
 git init
-git remote add origin https://github.com/Kabir1240/Learning-Web-Development.git
+git remote add origin https://github.com/Kabir1240/Hello-World.git
+git branch -M main
 git pull --set-upstream --allow-unrelated-histories origin main
 git add .
-git commit -m "first commit"
-git branch -M main
-git push -u origin main
+git commit -m "initial commit"
+git push
