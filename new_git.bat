99b881a30298cd99f6c5d5bb3e38ce063b5c54dc
Kabir Kashif <kashif.kabir60@gmail.com>
2024-07-10 12:21:29 +0500

    initial commit

---
+++ new_git.bat
@@ -0,0 +1,7 @@
+git init
+git remote add origin https://github.com/Kabir1240/Learning-Web-Development.git
+git pull --set-upstream --allow-unrelated-histories origin main
+git add .
+git commit -m "first commit"
+git branch -M main
+git push -u origin main
