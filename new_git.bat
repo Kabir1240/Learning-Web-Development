fa3512ff092dd660dff1ed0143f04ad0aa309c16
Kabir Kashif <kashif.kabir60@gmail.com>
2024-11-12 07:51:21 -0800

    removed new_git.bat from repo

--- new_git.bat
+++
@@ -1,7 +0,0 @@
-git init
-git remote add origin https://github.com/Kabir1240/Hello-World.git
-git branch -M main
-git pull --set-upstream --allow-unrelated-histories origin main
-git add .
-git commit -m "initial commit"
-git push
